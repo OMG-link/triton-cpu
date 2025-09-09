9bdb92fc7953c76099721fd7e89d1d8f815024ed
Kai Lin <omg_link@qq.com>
2025-09-09 18:09:40 +0800

    remove constraint that dims must be power of 2

--- lib/Dialect/Triton/IR/Traits.cpp
+++ lib/Dialect/Triton/IR/Traits.cpp
@@ -97,10 +97,6 @@ LogicalResult OpTrait::impl::verifyTensorSize(Operation *op) {
         return op->emitError("Maximum allowed number of elements is ")
                << maxTensorNumElements << ", but " << *op
                << " has more than that";
-      if ((numElements & (numElements - 1)) != 0)
-        return op->emitError("Number of elements must be power-of-two, but ")
-               << *op << " doesn't follow the rule (" << numElements << ")"
-               << " elements";
     }
   }
   for (auto opType : op->getResultTypes()) {
@@ -112,10 +108,6 @@ LogicalResult OpTrait::impl::verifyTensorSize(Operation *op) {
         return op->emitError("Maximum allowed number of elements is ")
                << maxTensorNumElements << ", but " << *op
                << " has more than that";
-      if ((numElements & (numElements - 1)) != 0)
-        return op->emitError("Number of elements must be power-of-two, but ")
-               << *op << " doesn't follow the rule (" << numElements << ")"
-               << " elements";
     }
   }
   return success();
