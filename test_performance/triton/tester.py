1fde88c4ab816e5a9e1b84c30ee119fe40b5a9a9
Kai Lin <omg_link@qq.com>
2025-12-01 21:25:16 +0800

    [Test] Make warmup exactly the same as actual measure.

--- test_performance/triton/tester.py
+++ test_performance/triton/tester.py
@@ -53,7 +53,9 @@ def performance_test(kernel, test_shapes):
 
             repeats = max(1, int(2**36 / (m * k * n)))
             params = kernel.prepare(m, k, n)
-            kernel.run(params, repeats=1) # Warmup
+            # Warmup (repeats must be exacly 'repeats' because this is part of hash key of kernel cache)
+            kernel.run(params, repeats=repeats)
+            # Measure
             elapsed = kernel.run(params, repeats=repeats)
             exp_cycles = kernel.expected_cycles(m, k, n)
             perf_ratio = elapsed * 1.6e9 / exp_cycles
