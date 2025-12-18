88be63f63abd1d89b7f70d03209b9fc25c126bf4
Kai Lin <omg_link@qq.com>
2025-12-18 20:04:58 +0800

    [Test] bring back deleted tests

--- test_performance/triton/tester.py
+++ test_performance/triton/tester.py
@@ -96,6 +96,36 @@ TEST_SETS = {
 
 KERNELS = [
     {
+        "kernel": InnerGEMM(in_dtype="i8", out_dtype="i16", MR=4, NR=4),
+        "correctness": ["correctness"],
+        "performance": ["normal_1"],
+    },
+    {
+        "kernel": OuterGEMM(in_dtype="i8", out_dtype="i16", MR=4, NR=32),
+        "correctness": ["correctness"],
+        "performance": ["normal_1"],
+    },
+    {
+        "kernel": OuterGEMM(in_dtype="i8", out_dtype="i16", MR=8, NR=32),
+        "correctness": ["correctness"],
+        "performance": ["normal_1"],
+    },
+    {
+        "kernel": OuterGEMM(in_dtype="i8", out_dtype="i16", MR=16, NR=16),
+        "correctness": ["correctness"],
+        "performance": ["normal_1"],
+    },
+    {
+        "kernel": Q4K_Q8K_GEMM(MR=4, NR=32),
+        "correctness": ["correctness"],
+        "performance": ["q4k_q8k_1"],
+    },
+    {
+        "kernel": Q4K_Q8K_GEMM(MR=12, NR=32),
+        "correctness": ["correctness"],
+        "performance": ["q4k_q8k_2"],
+    },
+    {
         "kernel": TransposeKernel(dtype="i8", TN=4, TM=8),
         "correctness": ["transpose_correctness"],
         "performance": [],
